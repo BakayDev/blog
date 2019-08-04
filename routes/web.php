98e19cbf098dc47c64e5b489eb86bcbd5b1e528a
BakayDev <gamempvs@gmail.com>
2019-08-04 14:34:00 +0600

    Removed welcome page

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,6 @@
 <?php
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index')->name('home');
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
